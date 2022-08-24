f0cf30524db4456c3d501e3bb9e27f545906da60
shaitemp88 <105381212+shaitemp88@users.noreply.github.com>
2022-08-24 17:06:53 +0300

    Update run_app.py

--- Code/run_app.py
+++ Code/run_app.py
@@ -5,35 +5,26 @@ import boto3
 # import uuid
 
 # Project configuration settings - Environment variables
-# AWS_ID = os.environ['aws_access_key_id']
-# AWS_KEY = os.environ['aws_secret_access_key']
-# REGION = os.environ['REGION']
-# AWS_ID = os.environ.get('aws_access_key_id')
-# AWS_KEY = os.environ.get('aws_secret_access_key')
-# REGION = os.environ.get('region')
-INTERN = "123"
-print(INTERN)
+AWS_ID = os.environ['aws_access_key_id']
+AWS_KEY = os.environ['aws_secret_access_key']
+REGION = os.environ['region']
 INTERVAL = os.environ["INTERVAL"]
-print(INTERVAL)
-
-TEST = "300"
-TEST = int(TEST)
-print(TEST)
-
 
 print("=                   =")
 print("Envoronment Variable:")
 print(AWS_ID)
 print(AWS_KEY)
 print(REGION)
+print(INTERVAL)
 print("=====================")
 
 
-ec2_resource = boto3.resource('ec2', region_name=REGION)
-instances = ec2_resource.instances.filter(
-    Filters=[{'Name': 'tag:k8s.io/role/master', 'Values': [1]}, {'Name': 'instance-state-code', 'Values': [16]}])
+ec2 = boto3.resource('ec2', region_name=REGION)
+instances = ec2.instances.filter(Filters=[{'Name': 'tag:k8s.io/role/master', 'Values': ["1"]}, {'Name': 'instance-state-code', 'Values': ["16"]}])
 for instance in instances:
-    print(instance.id, instance.instance_type)
+    for tag in instance.tags:
+        print(tag)
+    print("instance id: ", instance.id)
 
 
 print("===============!!!FIINSHED!!!===============")
