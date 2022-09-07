c983b36ffef67c419f858086717516613567b899
shaitemp88 <105381212+shaitemp88@users.noreply.github.com>
2022-09-07 20:10:49 +0300

    time loop

--- Code/run_app.py
+++ Code/run_app.py
@@ -1,6 +1,7 @@
 # Modules import
 import os
 import boto3
+import Time
 import pythonjsonlogger
 from pythonjsonlogger import jsonlogger
 
@@ -8,7 +9,7 @@ from pythonjsonlogger import jsonlogger
 AWS_ID = os.environ['aws_access_key_id']
 AWS_KEY = os.environ['aws_secret_access_key']
 REGION = os.environ['region']
-INTERVAL = os.environ["INTERVAL"]
+INTERVAL = int(os.environ["INTERVAL"])
 
 print("=                   =")
 print("Envoronment Variable:")
@@ -18,13 +19,13 @@ print(REGION)
 print(INTERVAL)
 print("=====================")
 
-
-ec2 = boto3.resource('ec2', region_name=REGION)
-instances = ec2.instances.filter(Filters=[{'Name': 'tag:k8s.io/role/master', 'Values': ["1"]}, {'Name': 'instance-state-code', 'Values': ["16"]}])
-for instance in instances:
-    for tag in instance.tags:
-        print(tag)
-    print("instance id: ", instance.id)
-
+while true:
+    ec2 = boto3.resource('ec2', region_name=REGION)
+    instances = ec2.instances.filter(Filters=[{'Name': 'tag:k8s.io/role/master', 'Values': ["1"]}, {'Name': 'instance-state-code', 'Values': ["16"]}])
+    for instance in instances:
+        for tag in instance.tags:
+            print(tag)
+        print("instance id: ", instance.id)
+    time.sleep(INTERVAL)
 
 print("===============!!!FIINSHED!!!===============")
