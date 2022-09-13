6a3d5e74eaf093fe7a725fdf33fcda3538fe2515
shaitemp88 <105381212+shaitemp88@users.noreply.github.com>
2022-09-13 10:18:26 +0300

    test pylint suggestions

--- Code/run_app.py
+++ Code/run_app.py
@@ -1,7 +1,7 @@
 # Modules import
 import os
-import boto3
 import time
+import boto3
 import pythonjsonlogger
 from pythonjsonlogger import jsonlogger
 
@@ -11,17 +11,18 @@ ACCESS_KEY = os.environ['aws_secret_access_key']
 REGION = os.environ['region']
 INTERVAL = int(os.environ["INTERVAL"])
 
-print("=                   =")
-print("Envoronment Variable:")
-print(ACCESS_ID)
-print(ACCESS_KEY)
-print(REGION)
-print(INTERVAL)
-print("=====================")
+print "=                   ="
+print "Envoronment Variable:"
+print ACCESS_ID
+print ACCESS_KEY
+print REGION
+print INTERVAL
+print "====================="
 
 while True:
     ec2 = boto3.resource('ec2', region_name=REGION, aws_access_key_id=ACCESS_ID, aws_secret_access_key= ACCESS_KEY)
-    #instances = ec2.instances.filter(Filters=[{'Name': 'tag:k8s.io/role/master', 'Values': ["1"]}, {'Name': 'instance-state-code', 'Values': ["16"]}])
+    #instances = ec2.instances.filter(Filters=[{'Name': 'tag:k8s.io/role/master', 'Values': ["1"]},
+                                                {'Name': 'instance-state-code', 'Values': ["16"]}])
     #instances = ec2.instances.all()
     instances = ec2.instances.filter(
         Filters=[
@@ -43,8 +44,8 @@ while True:
     for instance in instances:
         for tag in instance.tags:
             print(tag)
-        print("instance id: ", instance.id)
-    print("Done - waiting for next interval")
+        print "instance id: ", instance.id
+    print "Done - waiting for next interval"
     time.sleep(INTERVAL)
 
-print("===============!!!FIINSHED!!!===============")
+print "===============!!!FIINSHED!!!==============="
